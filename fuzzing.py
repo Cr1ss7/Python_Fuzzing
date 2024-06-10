00fe1d265628fd95e1a2f14bf6f0cb285dd3fc3e
Cristian Pineda <pineda.cristian@hotmail.com>
2024-06-09 19:02:47 -0600

    Adicion de los archivos de el proyecto

---
+++ fuzzing.py
@@ -0,0 +1,38 @@
+import requests
+
+def fuzzingSearchCommon(url):
+    url = veriUrl(url)
+    print("Realizando Fuzzing a -> "+url)
+    with open("big.txt", "r") as tf:
+        lines = tf.read().split("\n")
+
+    for line in lines:
+        full_url = url+"/"+line
+        resp = requests.get(full_url)
+        if resp.status_code == 200 or resp.status_code == 201 or resp.status_code == 202 or resp.status_code == 203:
+            print(full_url+" -> EXISTENTE")
+
+def fuzzingSearchDB(url):
+    url = veriUrl(url)
+    print("Realizando Fuzzing a -> "+url)
+    with open("sqli.auth.bypass.txt", "r") as tf:
+        lines = tf.read().split("\n")
+
+    for line in lines:
+        full_url = url+"/"+line
+        resp = requests.get(full_url)
+        if resp.status_code == 200 or resp.status_code == 201 or resp.status_code == 202 or resp.status_code == 203:
+            print(full_url+" -> EXISTENTE")
+
+def fuzzingSearhExt(url):
+    resp = requests.get(url)
+    print(resp)
+
+#Funcion para verificar url
+def veriUrl(url):
+    if url[-1] == "/":
+        url = url[:-1]
+    return url
+
+#Diccionarios extraidos de https://github.com/danielmiessler/SecLists/tree/master/Fuzzing
+#Diccionarios extraidos de https://github.com/00xBAD/kali-wordlists/
---
+++ main.py
@@ -0,0 +1,18 @@
+import fuzzing
+
+print("Digite la URL a realizar fuzzing")
+url = input()
+
+print("Seleccione el tipo de fuzzing a realizar\n1. Comun\n2. Bases de datos\n3. Extencion de archivos")
+tipo = input()
+tipo = int(tipo)
+
+match tipo:
+    case 1:
+        fuzzing.fuzzingSearchCommon(url) 
+    case 2:
+        fuzzing.fuzzingSearchDB(url)
+    case 3:
+        fuzzing.fuzzingSearhExt(url)
+    case other:
+        print("Seleccione una opcion valida")
