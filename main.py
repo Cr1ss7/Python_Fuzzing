082605c33f15e7df9f5f3043426bcdc79a45e672
Cristian Pineda <pineda.cristian@hotmail.com>
2024-06-09 23:14:33 -0600

    Actualizacion de el archivo main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import fuzzing
 print("Digite la URL a realizar fuzzing")
 url = input()
 
-print("Seleccione el tipo de fuzzing a realizar\n1. Comun\n2. SQLI\n3. Extencion de archivos")
+print("Seleccione el tipo de fuzzing a realizar\n1. Comun (Usa palabras que son comunes para directorios)\n2. SQLI (Para descubrir vulnerabilidades de Inyecciones SQL)\n3. Extencion de archivos (En desarrollo)")
 tipo = input()
 tipo = int(tipo)
 
@@ -12,7 +12,8 @@ match tipo:
         fuzzing.fuzzingSearchCommon(url) 
     case 2:
         fuzzing.fuzzingSearchDB(url)
-    #case 3:
+    case 3:
+        print("Funcion en desarrollo")
         #fuzzing.fuzzingSearhExt(url)
     case other:
         print("Seleccione una opcion valida")
